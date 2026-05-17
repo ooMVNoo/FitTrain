7ff6948e3b270e3b4bbc93aca5964f4ab236c490
AymanFitouri <100640018+ooMVNoo@users.noreply.github.com>
2026-05-17 19:20:46 +0200

    Update FitTrain.h

--- FitTrain.h
+++ FitTrain.h
@@ -12,7 +12,7 @@ class FitTrain{
 
     bool val_in_tab(int val, vector<int>tab);//verifie qu'une valeur est dans un tableau
     vector<float> linearize(vector<vector<float>> tab); //transforme un tableau importé d'un CSV ( qui donne un vecteur de vecteur en vecteur 1D)
-    int linIndex(int i,int j);//linearise l'index : 2D->1D [i][j]->[k=i*nb_colonne_tableau+j]
+    virtual int linIndex(int i,int j)=0;//linearise l'index : 2D->1D [i][j]->[k=i*nb_colonne_tableau+j]
 
 
 };
